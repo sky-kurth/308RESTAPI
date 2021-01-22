0d6a0b32a1301de6bb3645618c636fd771521ea8
sky-kurth <sekurth@calpoly.edu>
2021-01-21 23:15:33 -0800

    Working mostly

--- sample_backend.py
+++ sample_backend.py
@@ -1,19 +1,42 @@
+import random
 from flask import Flask
 from flask import request
 from flask import jsonify
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/users/<id>')
+#@app.route('/users')
+#def get_filtered_user():
+#   search_username = request.args.get('name') #accessing the value of parameter 'name'
+#   search_job = request.args.get('job')
+#   if search_username :
+#      subdict = {'users_list' : []}
+#      for user in users['users_list']:
+#         #if user['name'] == search_username:
+#            #subdict['users_list'].append(user)
+#         if user['job'] == search_job:
+#            subdict['users_list'].append(user)
+#      return subdict
+#   return users
+
+@app.route('/users/<id>', methods=['GET', 'DELETE'])
 def get_user(id):
    if id :
       for user in users['users_list']:
         if user['id'] == id:
-           return user
-      return ({})
+            if request.method == 'GET':
+               return user
+            elif request.method == 'DELETE':
+                users['users_list'].remove(user) #removes user, 204 code
+                resp = jsonify({}), 204
+                return resp
+      resp = jsonify({"error": "User not found"}), 404 #jsonify built in, converts object to json
+      return resp
    return users
 
 @app.route('/users', methods=['GET', 'POST'])
@@ -29,11 +52,23 @@ def get_users():
       return users
    elif request.method == 'POST':
       userToAdd = request.get_json()
+      userToAdd['id'] = generate_id()
       users['users_list'].append(userToAdd)
-      resp = jsonify(success=True)
-      #resp.status_code = 200 #optionally, you can always set a response code. 
+      #resp = jsonify(success=True)
+      resp = jsonify(userToAdd), 201
+      #resp.status_code = 201
+      #optionally, you can always set a response code. 
       # 200 is the default code for a normal response
       return resp
+
+def generate_id():
+    newId = ""
+    for i in range(3):
+        newId += chr(random.randint(97,122))
+    for i in range(3):
+        newId += chr(random.randint(48,57))
+    return newId
+    #return "abc123"
 
 users = { 
    'users_list' :
@@ -45,6 +80,11 @@ users = {
       },
       {
          'id' : 'abc123', 
+         'name': 'Mac',
+         'job': 'Bouncer',
+      },
+      {
+         'id' : 'def456', 
          'name': 'Mac',
          'job': 'Bouncer',
       },
